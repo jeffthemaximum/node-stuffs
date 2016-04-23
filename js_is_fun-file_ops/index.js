313780d003ab0c81c42420ec2b0ebe7e1824e20e
jeffthemaximum <frey.maxim@gmail.com>
2016-04-23 15:39:16 -0400

    it works at basic level. MOAR BETTER

--- js_is_fun-file_ops/index.js
+++ js_is_fun-file_ops/index.js
@@ -1,37 +1,10 @@
 //main script here
 var fs = require('fs');
-
-
-// professional node book
-
-// fs.open('files/original/anothertext-file.txt', 'r+', function opened(err, fd) {
-    
-//     console.log("opened");
-    
-//     if (err) { throw err }
-    
-//     var readBuffer = new Buffer(1024),
-//         bufferOffset = 0,
-//         bufferLength = readBuffer.length,
-//         filePosition = 0;
-
-//     fs.read(fd,
-//             readBuffer,
-//             bufferLength,
-//             filePosition,
-//             function read(err, readBytes) {
-//                 if (err) { throw err; }
-//                 console.log('just read ' + readBytes + ' bytes');
-//                 if (readBytes > 0) {
-//                     console.log(readBuffer.slice(0, readBytes));
-//                 }
-//             });
-
-// });
-
+var datetime = require('node-datetime');
+// https://nodejs.org/docs/latest/api/globals.html#globals_dirname
+var dirname = __dirname + "/files/original/"
 
 var isJsonString = function(str) {
-    debugger;
     try {
         JSON.parse(str);
     } catch (e) {
@@ -40,10 +13,17 @@ var isJsonString = function(str) {
     return true;
 }
 
+// make new dir
+// do this synchronously so you're sure it happens first
+var makeDir = function(dirPath) {
+    fs.mkdir(dirPath);
+}
 
-// https://nodejs.org/docs/latest/api/globals.html#globals_dirname
-var dirname = __dirname + "/files/original/"
-console.log(dirname);
+var generateFileName = function(filename) {
+    var currDateTime = datetime.create().format('y-m-d_H-S');
+    // http://stackoverflow.com/questions/4250364/how-to-trim-a-file-extension-from-a-string-in-javascript
+    return(filename.replace(/\.[^/.]+$/, "") + "_EDITED_" + currDateTime)
+}
 
 // open all files in directory
 // from http://stackoverflow.com/questions/10049557/reading-all-files-in-a-directory-store-them-in-objects-and-send-the-object
@@ -59,35 +39,47 @@ function readFiles(dirname, onFileContent, onError) {
         filenames.forEach(function(filename) {
 
             var wholePath = dirname + filename;
+            var newFileName = generateFileName(filename);
+            var newWholePath = __dirname + "/files/moved/" + newFileName;
 
             fs.readFile(wholePath, 'utf8', function(err, contents) {
 
+                //do json stuff
                 if (isJsonString(contents)) {
-
                     var myJson = JSON.parse(contents)
                     myJson['original-path-name'] = dirname;
                     myJson['original-file-name'] = filename;
+                    myJson['new-file-path'] = newWholePath;
+                    myJson['new-file-name'] = newFileName;
+                    // likely shouldnt be async
                     fs.writeFile(wholePath, JSON.stringify(myJson), function(err) {
                         if (err) throw err;
                     })
 
+                //do string stuff
                 } else {
-                    myStr = "\n" + filename + "\n"
+                    var myStr = "\n" + filename + "\n"
                     myStr += dirname + "\n"
+                    myStr += newWholePath + "\n"
+                    myStr += newFileName
                     //write those 4 things to file
+                    // likely shouldnt be async
                     fs.appendFile(wholePath, myStr, function (err) {
                         if (err) throw err;
                     });
 
                 }
 
-
+                // do file move
+                fs.rename(wholePath, newWholePath, function(err) {
+                    if (err) throw err;
+                })
             });
         })
     })
 }
 
+// do this synchronously so it happens first
+makeDir(__dirname + "/files/moved/");
+// do this asynchronous so it's fast
 readFiles(dirname);
-// read file
-// write to file
-// move to new location
